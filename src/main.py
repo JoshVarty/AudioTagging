74bdbf156dc845f552d9f2b92a6d62d99c0295e9
joshvarty@gmail.com <joshvarty@gmail.com>
2019-04-11 23:49:48 -0400

    Initial setup for main driver

--- src/main.py
+++ src/main.py
@@ -45,11 +45,15 @@ test = pd.read_csv('../data/sample_submission.csv')
 X = train['fname']
 y = train['labels'].apply(lambda f: f.split(','))
 transformed_y = MultiLabelBinarizer().fit_transform(y)
+filenames = train['fname'].values
+filenames = filenames.reshape(-1, 1)
 
-mskf = MultilabelStratifiedKFold(n_splits=5, random_state=4, shuffle=True)
+oof_preds = np.zeros((len(train), 80))
+test_preds = np.zeros((len(test), 80))
 
 tfms = get_transforms(do_flip=True, max_rotate=0, max_lighting=0.1, max_zoom=0, max_warp=0.)
 
+mskf = MultilabelStratifiedKFold(n_splits=5, random_state=4, shuffle=True)
 for train_index, val_index in mskf.split(X, transformed_y):
 
     #Our clasifier stuff    
@@ -61,9 +65,14 @@ for train_index, val_index in mskf.split(X, transformed_y):
 
     f_score = partial(fbeta, thresh=0.2)
     learn = cnn_learner(data, models.resnet18, pretrained=False, metrics=[f_score, calculate_overall_lwlrap_sklearn])
-    learn.fit_one_cycle(1, slice(1e-6, 1e-1))
+    learn.fit_one_cycle(5, slice(1e-6, 1e-1))
     learn.unfreeze()
-    learn.fit_one_cycle(1, slice(1e-6, 1e-1))
+    learn.fit_one_cycle(20, slice(1e-6, 1e-2))
+
+    val_preds, _ = learn.get_preds(ds_type=DatasetType.Valid)
+
+    oof_preds[val_index, :] = val_preds
+
     #Save learner
     learn.export()
     #Get test predictions
@@ -71,11 +80,20 @@ for train_index, val_index in mskf.split(X, transformed_y):
     learn = load_learner('../'/WORK/'image', test=test_src)
     preds, _ = learn.get_preds(ds_type=DatasetType.Test)
 
-    test[learn.data.classes] += preds
+    test_preds += preds.numpy()
 
-test[learn.data.classes] /= NFOLDS
+score = calculate_overall_lwlrap_sklearn(oof_preds, transformed_y).numpy()[0]
 
+print("Saving out-of-fold predictions...")
+all_oof_preds = pd.DataFrame(np.hstack((filenames, oof_preds)), columns = test.columns)
+all_oof_preds.to_csv('../kfolds/{}__{}.csv'.format(MODEL_NAME, str(score)), index=False)
 
-test.to_csv('submission.csv', index=False)
+print("Saving code...")
+shutil.copyfile(os.path.basename(__file__), '../model_source/{}__{}.py'.format(MODEL_NAME, str(score)))
 
+print("Saving submission file...")
+# Adjust test predictions for number of folds and save
+test_preds /= NFOLDS
+test[learn.data.classes] = test_preds
+test.to_csv('../model_predictions/submission_{}__{}.csv'.format(MODEL_NAME, str(score)), index=False)
 
